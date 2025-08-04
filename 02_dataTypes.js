77bb2fce1c012136f2ce73435c1ef30e57de40c3
Nirakar Naik <116417014+NirakarNaik@users.noreply.github.com>
2025-08-04 17:52:10 +0000

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3)
+console.log("Ashish")
+
+let name = "Ashish"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// TYPES OF DATA TYPES:
+
+// number => 2 to the power of 53 (range of numbers that can be used)
+// bigint (used for big integer data)
+// string => " "
+// boolen => true/false
+// null => standalone value
+// undefined => 
+// symbol => mostly used to find uniqueness
+
+// object
+
+console.log(typeof "Ashish");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
