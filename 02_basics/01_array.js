5d29a997cae7556f8b152ee312f3ff9a0ad6e555
Nirakar Naik <116417014+NirakarNaik@users.noreply.github.com>
2025-08-12 17:01:03 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,32 @@
+// ARRAY
+
+const myArr = [0,1,2,3,4,5,true,'ashish']
+console.log(myArr[7]);
+
+const myHeros = ['shaktiman','naagraj']
+
+const myArr2 = new Array(1,2,3,4)
+
+// ARRAY METHODS
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop() // removes last value
+console.log(myArr);
+
+myArr.unshift(9) // adds 9 in 0 index
+console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+
+
+
+
+
+
+
+
+
+
