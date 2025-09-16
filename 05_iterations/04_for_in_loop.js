6dbe5a72be883cb8116fc5ed4734b39a29f4db45
Nirakar Naik <116417014+NirakarNaik@users.noreply.github.com>
2025-09-16 19:20:01 +0000

    for in

---
+++ 05_iterations/04_for_in_loop.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js: 'javascript',
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "Swift by Apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js","ruby","py","java","cpp"]
+for (const key in programming) {
+    console.log(programming[key]); 
+}
+
+
+
